fd4cfc8ceeeb0db273700f1d522f89a623cfb8d2
upsu9 <upsu10@gmail.com>
2021-09-24 20:53:33 +0530

    final without css

---
+++ app.js
@@ -0,0 +1,85 @@
+var inputPast = document.querySelector("#inputPast");
+var inputStock = document.querySelector("#inputStock");
+var inputCurrent = document.querySelector("#inputCurrent");
+var inputError = document.querySelector("#inputError");
+var finalOutput = document.querySelector("#finalOutput");
+var inputBtn = document.querySelector("#inputBtn");
+var clrBtn = document.querySelector("#clrBtn");
+
+
+inputBtn.addEventListener("click", clickHandler);
+clrBtn.addEventListener("click", clear);
+
+
+function clickHandler() {
+    var inputPrice = Number(inputPast.value);
+    var stocksQuantity = Number(inputStock.value);
+    var currentPrice = Number(inputCurrent.value);
+    check(inputPrice, stocksQuantity, currentPrice)
+
+}
+
+function check(pastPrice, stocks, currPrice) {
+
+
+    /* if( (inputPrice || stocksQuantity || currentPrice === 0 || "" ) || ( inputPrice || stocksQuantity || currentPrice < 0) ){ */
+    if (pastPrice <= 0 || stocks <= 0 || currPrice <= 0) {
+        errormsg("Please put Valid input (ie. All fields shall be more than zero)")
+    } else {
+        calculator(pastPrice, stocks, currPrice);
+
+    }
+
+}
+
+function calculator(inputPrice, stocksQuantity, currentPrice) {
+    if (inputPrice < currentPrice) {
+        var profit = (currentPrice - inputPrice) * stocksQuantity;
+        var profitPercentage = (profit / inputPrice) * 100
+        var proPer = profitPercentage.toFixed(2)
+        showmsg(`You have made profit of Rs. ${profit} and  percentage ${proPer} % .`)
+    } else {
+        if (inputPrice > currentPrice) {
+            var loss = (inputPrice - currentPrice) * stocksQuantity;
+            var lossPercentage = (loss / inputPrice) * 100
+            var lossPer = lossPercentage.toFixed(2)
+            showmsg(`You have made Rs. ${loss} and  percentage ${lossPer} % .`)
+
+        } else {
+            if (inputPrice = currentPrice);
+            showmsg(`Have faith, Destiny is all.`)
+
+        }
+    }
+}
+
+
+function clear() {
+    inputPast.value = "";
+    inputStock.value = "";
+    inputCurrent.value = "";
+    clrErrorMsg();
+    hidemsg();
+}
+
+function showmsg(msg) {
+    finalOutput.style.display = "block";
+    finalOutput.style.color ="green"
+    finalOutput.innerText = msg;
+    
+
+}
+
+function errormsg(msg) {
+    inputError.style.display = "block";
+    inputError.style.color ="Red"
+    inputError.innerText = msg;
+}
+
+function clrErrorMsg() {
+    inputError.style.display = "none";
+}
+
+function hidemsg() {
+    finalOutput.style.display = "none";
+}
